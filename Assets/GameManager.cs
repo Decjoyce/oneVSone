f2b4f0b6bacd4acafb62ce99915f24c4fd36019c
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2023-09-23 14:40:19 +0100

    Testing

---
+++ Assets/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    int score_P1;
+    int score_P2
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void IncreaseScore_P1()
+    {
+        score_P1++;
+    }
+
+    public void IncreaseScore_P2()
+    {
+        score_P2++;
+    }
+
+}
