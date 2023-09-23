59c2d7b6899948e663d3de46ad93e60dc0526f72
AAROnmn <aaronmccabe212@gmail.com>
2023-09-23 17:18:09 +0100

    MovementCode

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class playermovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
+    public float moveSpeed;
+
+    public Rigidbody2D rb;
+
+    private Vector2 MoveDirection;
+   
+    
     // Update is called once per frame
     void Update()
     {
-        
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        MoveDirection = new Vector2(moveX, moveY);
+    }
+    void Move()
+    {
+        rb.velocity = new Vector2(MoveDirection.x  * moveSpeed, MoveDirection.y * moveSpeed);
     }
 }
