2a5c4f4953f65bef4897a237637f0f18a587f91e
Maximglkv <91562210+Maximglkv@users.noreply.github.com>
2023-09-28 19:14:21 +0100

    bullet bounce attempt 1

--- Assets/Scripts/gun version/v1/weap.cs
+++ Assets/Scripts/gun version/v1/weap.cs
@@ -6,7 +6,7 @@ public class Weapon : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform firePoint;
-    public float fireForce = 20f;
+    public float fireForce = 10f;
 
     public void Fire()
     {
@@ -21,5 +21,5 @@ public class Weapon : MonoBehaviour
         bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
     }
     */
-    
+
 }
