d1b15889c12d7112421f984be6f58fe6b18fc558
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2023-10-09 13:02:46 +0100

    sdws

--- Assets/Scripts/DestroyTimer.cs
+++ Assets/Scripts/DestroyTimer.cs
@@ -9,6 +9,13 @@ public class DestroyTimer : MonoBehaviour
 
     [SerializeField]
     bool waitUntilStart = true;
+
+    [SerializeField]
+    bool disable = true;
+
+    [SerializeField]
+     private GameObject objectToDisable;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +26,23 @@ public class DestroyTimer : MonoBehaviour
     private void Update()
     {
         if(waitUntilStart && GameManager.instance.gameStarted && !GameManager.instance.roundOver)
-            Destroy(gameObject, timer);
+        {
+            if (!disable)
+                Destroy(gameObject, timer);
+            else if(objectToDisable.activeSelf)
+                StartCoroutine(DisableObject());
+        }
+            
+
+        if(GameManager.instance.gameStarted && GameManager.instance.roundOver && !objectToDisable.activeSelf)
+        {
+            objectToDisable.SetActive(true);
+        }
+    }
+
+    IEnumerator DisableObject()
+    {
+        yield return new WaitForSeconds(timer);
+        objectToDisable.gameObject.SetActive(false);
     }
 }
