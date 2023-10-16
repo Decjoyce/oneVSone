00db36b45342338b0434f9577b7d2d16bb645472
Decjoyce <118187009+Decjoyce@users.noreply.github.com>
2023-10-16 12:44:51 +0100

    oppop

---
+++ Assets/SpawnTimer.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTimer : MonoBehaviour
+{
+    [SerializeField]
+    float timer;
+
+    [SerializeField]
+    bool waitUntilStart = true;
+
+    [SerializeField]
+    bool disable = true;
+
+    [SerializeField]
+    private GameObject objectToEnable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!waitUntilStart)
+            Destroy(gameObject, timer);
+    }
+
+    private void Update()
+    {
+        if (waitUntilStart && GameManager.instance.gameStarted && !GameManager.instance.roundOver)
+        {
+            if (!objectToEnable.activeSelf)
+                StartCoroutine(EnableObject());
+        }
+
+
+        if (GameManager.instance.gameStarted && GameManager.instance.roundOver && objectToEnable.activeSelf)
+        {
+            objectToEnable.SetActive(false);
+        }
+    }
+
+    IEnumerator EnableObject()
+    {
+        yield return new WaitForSeconds(timer);
+        objectToEnable.gameObject.SetActive(true);
+    }
+}
